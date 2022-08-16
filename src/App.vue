fd1cc9bafb6a9988db6b26c32074f64f0776ac35
lhdai <d_muses@163.com>
2022-08-16 15:43:07 +0800

    添加tooltip组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <div class="m-tag yellow" @click="onShowMessage('This is a normal message')">哥斯拉</div>
+    <div class="u-tag yellow mr20" @mouseenter="onShowTip($event, '特斯拉(Tesla)是美国一家电动汽车及能源公司，总部位于帕洛阿托(Palo Alto)，市值达2100亿美元，产销电动汽车、太阳能板、及储能设备')" @mouseleave="onHideTip">特斯拉</div>
+		<div class="u-tag blue" @mouseenter="onShowTip($event, '《哥斯拉大战金刚》是由美国传奇影业公司出品，亚当·温佳德执导，亚历山大·斯卡斯加德、米莉·博比·布朗、丽贝卡·豪尔、凯莉·霍特尔、布莱恩·泰里·亨利、小栗旬联合主演的动作科幻片，于2021于3月26日在中国内地上映')" @mouseleave="onHideTip">哥斯拉</div>
+    <br/>
+    <div class="u-tag yellow" @click="onShowMessage('This is a normal message')">哥斯拉</div>
+    <Tooltip ref="tooltip" :maxWidth="240">
+			<div class="u-content" v-if="content">{{ content }}</div>
+		</Tooltip>
     <Message ref="message" :duration="3000" :top="30" />
   </div>
 </template>
 <script>
+import Tooltip from '@/components/Tooltip'
 import Message from '@/components/Message'
 export default {
   name: 'App',
   components: {
+    Tooltip,
     Message
   },
+  data () {
+    return {
+      content: ''
+    }
+  },
   methods: {
+    onShowTip (e, content) {
+      this.content = content
+      this.$refs.tooltip.show(e.target.offsetTop, e.target.offsetLeft, e.target.offsetWidth)
+      console.log('元素距页面顶部的距离offsetTop:', e.target.offsetTop)
+      console.log('元素距页面左边界的距离offsetLeft:', e.target.offsetLeft)
+      console.log('元素自身宽度offsetWidth:', e.target.offsetWidth)
+      console.log('元素自身高度offsetHeight:', e.target.offsetHeight)
+      console.log('元素标签内容innerText:', e.target.innerText)
+    },
+    onHideTip () {
+      this.$refs.tooltip.onHide()
+    },
     onShowMessage (content) {
       this.$refs.message.info(content) // info调用
       // this.$refs.message.success(content) // success调用
@@ -23,15 +48,10 @@ export default {
 </script>
 <style lang="less" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 150px;
 }
-.m-tag {
-  margin-top: 120px;
+.u-tag {
+  margin-top: 60px;
   display: inline-block;
   padding: 6px 12px;
   border-radius: 15px;
@@ -41,8 +61,15 @@ export default {
   line-height: 20px;
   cursor: pointer;
 }
+.mr20 {
+  margin-right: 20px;
+}
 .yellow {
   color: #FF9000;
   background: #FFF9F0;
+}
+.blue {
+  color: #0079DD;
+  background: #F0F7FD;
 }
 </style>
---
+++ src/components/Tooltip.vue
@@ -0,0 +1,104 @@
+<template>
+  <transition name="fade">
+    <div
+      ref="tooltip"
+      class="m-tooltip"
+      v-show="showTooltip"
+      @mouseenter="onShow"
+      @mouseleave="onHide"
+      :style="`max-width: ${maxWidth}px; top: ${top}px; left: ${left}px;`">
+      <div class="u-tooltip-content">
+        <slot>暂无内容</slot>
+      </div>
+      <div class="u-tooltip-arrow"></div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  name: 'Tooltip',
+  props: {
+    maxWidth: { // 提示框内容最大宽度
+      type: Number,
+      default: 120
+    }
+  },
+  data () {
+    return {
+      showTooltip: false,
+      hideTimer: null,
+      top: 0, // 提示框top定位
+      left: 0 // 提示框left定位
+    }
+  },
+  methods: {
+    show (targetTop, targetLeft, targetWidth) {
+      clearTimeout(this.hideTimer)
+      this.showTooltip = true
+      this.$nextTick(() => {
+        const tipWidth = this.$refs.tooltip.offsetWidth // 提示框元素宽度
+        const tipHeight = this.$refs.tooltip.offsetHeight // 提示框元素高度
+        console.log('tipWidth:', tipWidth)
+        console.log('tipHeight:', tipHeight)
+        this.top = targetTop - tipHeight
+        this.left = targetLeft - (tipWidth - targetWidth) / 2
+      })
+    },
+    onShow () {
+      clearTimeout(this.hideTimer)
+      this.showTooltip = true
+    },
+    onHide () {
+      this.hideTimer = setTimeout(() => {
+        this.showTooltip = false
+      }, 100)
+    },
+  }
+}
+</script>
+<style lang="less" scoped>
+// 渐变过渡效果
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+// 滑动渐变过渡效果
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateY(6px);
+  opacity: 0;
+}
+.m-tooltip {
+  position: absolute;
+  z-index: 999;
+  padding-bottom: 6px;
+  .u-tooltip-content {
+    padding: 10px;
+    margin: 0 auto;
+    word-break: break-all;
+    word-wrap: break-word;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 8px 0px rgba(0,121,221,0.3000);
+    border-radius: 6px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    color: #666666;
+  }
+  .u-tooltip-arrow {
+    margin: 0 auto;
+    width: 0;
+    height: 0;
+    border-left: 3px solid transparent;
+    border-right: 3px solid transparent;
+    border-top: 4px solid #FFF;
+  }
+}
+</style>
