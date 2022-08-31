e58f6bec160385e2ff29f78c53c5d206f725a0ef
lhdai <d_muses@163.com>
2022-08-31 17:55:21 +0800

    swiper

--- src/views/Swiper.vue
+++ src/views/Swiper.vue
@@ -12,8 +12,8 @@
     <!-- 如果需要分页器 -->
     <div class="swiper-pagination"></div>
     <!-- 如果需要导航按钮 -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div><!--左箭头。如果放置在swiper外面，需要自定义样式。-->
+    <div class="swiper-button-next"></div><!--右箭头。如果放置在swiper外面，需要自定义样式。-->
     <!-- 如果需要滚动条 -->
     <!-- <div class="swiper-scrollbar"></div> -->
   </div>
@@ -53,6 +53,7 @@ export default {
       // scrollbar: { // 如果需要滚动条
       //   el: '.swiper-scrollbar',
       // },
+      autoplay: true,
       loop: true // 循环模式选项
     })
   }
@@ -60,10 +61,21 @@ export default {
 </script>
 <style lang="less" scoped>
 .swiper-container {
+  --swiper-theme-color: #1890FF;/* 设置Swiper风格 */
+  --swiper-navigation-color: #1890FF;/* 单独设置按钮颜色 */
+  --swiper-navigation-size: 48px;/* 设置按钮高度 */
   width: 600px;
   height: 400px;
   margin: 0 auto;
-  .swiper-slide {
+  .swiper-prev { // 自定义导航样式
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 99;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background:aquamarine;
   }
 }
 </style>
