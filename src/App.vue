a17805793ca90cdbc40854bd266b262a249d1f20
lhdai <d_muses@163.com>
2022-12-05 09:38:28 +0800

    mourn

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,8 @@ export default {
   name: 'App',
   data () {
     return {
-      includes: []
+      includes: [],
+      mournDate: {}
     }
   },
   watch: {
@@ -26,6 +27,36 @@ export default {
       if (from.meta.keepAlive && (to.meta.depth || 0) < from.meta.depth) {
         const target = this.includes.indexOf(from.name)
         this.includes.splice(target, 1)
+      }
+    }
+  },
+  created () {
+    this.getMournDate()
+  },
+  methods: {
+    getMournDate () {
+      // 调用接口获取后台设置的追悼日期范围（起始日~截止日）
+      this.mournDate = {
+        start: 1669824000000, // 2022-12-01
+        end: 1670601600000 // 2022-12-10
+      }
+      this.checkMourn(this.mournDate)
+    },
+    checkMourn (mournDate) {
+      var now = new Date().getTime()
+      if (mournDate.start <= now && now <= mournDate.end) {
+        // 追悼日只有设置在html元素上才不会影响fixed和absolute定位
+        document.documentElement.style.filter = 'grayscale(100%)'
+        setTimeout(() => { // 自动取消黑白
+          document.documentElement.style.filter = 'none'
+        }, mournDate.end - now)
+      } else {
+        document.documentElement.style.filter = 'none'
+        if (now < mournDate.start) { // 自动变为黑白
+          setTimeout(() => {
+            document.documentElement.style.filter = 'grayscale(100%)'
+          }, mournDate.start - now)
+        }
       }
     }
   }
